77e61e1c4538f6038ef7835dfe5fc1b0bcffc8a2
nahal <mrym.mhdzde@gmail.com>
2024-12-01 16:26:32 +0330

    stats and filters

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,5 +1,6 @@
 import jobsModel from "../models/jobsModel.js";
 import mongoose from "mongoose";
+import moment from "moment";
 
 export const createJobController = async (req, res, next) => {
   const { company, position } = req.body;
@@ -54,4 +55,61 @@ export const deleteJobController = async (req, res, next) => {
   res.status(200).json({
     message: "Success, Job deleted!",
   });
+};
+
+export const jobStatsController = async (req, res, next) => {
+  const stats = await jobsModel.aggregate([
+    {
+      $match: {
+        createdBy: new mongoose.Types.ObjectId(req.user.userId),
+      },
+    },
+    {
+      $group: {
+        _id: "$status",
+        count: { $sum: 1 },
+      },
+    },
+  ]);
+  const defaultStats = {
+    pending: stats.pending || 0,
+    reject: stats.reject || 0,
+    interview: stats.interview || 0,
+  };
+
+  let monthlyApplication = await jobsModel.aggregate([
+    {
+      $match: {
+        createdBy: new mongoose.Types.ObjectId(req.user.userId),
+      },
+    },
+    {
+      $group: {
+        _id: {
+          year: { $year: "$createdAt" },
+          month: { $month: "$createdAt" },
+        },
+        count: {
+          $sum: 1,
+        },
+      },
+    },
+  ]);
+  monthlyApplication = monthlyApplication
+    .map((item) => {
+      const {
+        _id: { year, month },
+        count,
+      } = item;
+      const date = moment()
+        .month(month - 1)
+        .year(year)
+        .format("MMM Y");
+      return { date, count };
+    })
+    .reverse();
+
+  res
+    .status(200)
+    .json({ totalJob: stats.length, defaultStats, monthlyApplication });
 };
--- routes/jobsRoutes.js
+++ routes/jobsRoutes.js
@@ -4,6 +4,7 @@ import {
   createJobController,
   deleteJobController,
   getAllJobsController,
+  jobStatsController,
   updateJobController,
 } from "../controllers/jobsController.js";
 
@@ -13,5 +14,6 @@ router.post("/create-job", userAuth, createJobController);
 router.get("/get-job", userAuth, getAllJobsController);
 router.patch("/update-job/:id", userAuth, updateJobController);
 router.delete("/delete-job/:id", userAuth, deleteJobController);
+router.get("/job-stats", userAuth, jobStatsController);
 
 export default router;
