0748cfc59e9743aeec100765be1882db5f0f16c7
nahal <mrym.mhdzde@gmail.com>
2024-12-01 14:11:42 +0330

    userModel schema

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
+// packages imports
 import express from "express";
 import dotenv from "dotenv";
+import morgan from "morgan";
+//files imports
 import connectDB from "./config/db.js";
 
-dotenv.config()
+dotenv.config();
 
 connectDB();
 
 const app = express();
+
+app.use(express.json());
+app.use(morgan("dev"));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
