d6c56ad00411448dde201733cd98ee86fa191663
nahal <mrym.mhdzde@gmail.com>
2024-12-01 15:11:35 +0330

    error handling and hashing

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 // packages imports
 import express from "express";
+import "express-async-errors";
 import dotenv from "dotenv";
 import morgan from "morgan";
 // files imports
 import connectDB from "./config/db.js";
 // routes imports
 import authRoutes from "./routes/authRoutes.js";
+import errorMiddleware from "./middlewares/errorMiddleware.js";
 
 dotenv.config();
 
@@ -17,6 +19,8 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 app.use("/api/v1/auth", authRoutes);
+
+app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
