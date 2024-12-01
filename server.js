50f3656cc31b8d49f3305a71ad9fb21d7fd5ac4e
nahal <mrym.mhdzde@gmail.com>
2024-12-01 15:56:12 +0330

    User API

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import morgan from "morgan";
 import connectDB from "./config/db.js";
 // routes imports
 import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js"
 import errorMiddleware from "./middlewares/errorMiddleware.js";
 
 dotenv.config();
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/user", userRoutes);
 
 app.use(errorMiddleware);
 
